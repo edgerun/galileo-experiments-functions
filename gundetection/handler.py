04f7c67e2b88dbeeddc7f4b5b41569320b059ccb
Paul Prüller <>
2022-07-10 19:40:52 +0200

    Add taxidriversafetyapp function + gun detection function

---
+++ gundetection/handler.py
@@ -0,0 +1,72 @@
+import time
+import cv2
+import numpy as np
+cascadeUrl = "./function/model/cascade.xml"
+
+#based on https://www.geeksforgeeks.org/gun-detection-using-python-opencv/
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+    resized = cv2.resize(image, dim, interpolation = inter)
+    return resized
+
+def handle(event, context):
+    # load model
+    start_time = time.time()
+    try:
+        gun_cascade = cv2.CascadeClassifier(cascadeUrl)
+    except Exception as e:
+        return returnBadRequest("Error while model loading: " + str(e))
+    stop_time = time.time()
+    model_load_time = stop_time - start_time
+
+    # preprocess
+    start_time = time.time()
+    try:
+        nparr = np.fromstring(event.body, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        image = image_resize(image, width=500)
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        gray = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)
+        stop_time = time.time()
+    except Exception as e:
+        return returnBadRequest("Error while pre-processing: " + str(e))
+    preprocess_time = stop_time - start_time
+
+    # predict
+    start_time = time.time()
+    try:
+        gun = gun_cascade.detectMultiScale(gray, 1.3, 5, minSize = (100, 100))
+        gun_exist = False
+        if len(gun) > 0:
+            gun_exist = True
+        stop_time = time.time()
+    except Exception as e:
+        return returnBadRequest("Error while inference: " + str(e))
+    predict_time = stop_time - start_time
+
+    return {
+        "statusCode": 200,
+        "body": {
+            "model_load_time": model_load_time * 1000,
+            "preprocess_time": preprocess_time * 1000,
+            "predict_time": predict_time * 1000,
+            "results": str(gun),
+            "gunExist": gun_exist
+        }
+    }
+
+def returnBadRequest(msg):
+    return {
+        "statusCode": 400,
+        "body": msg
+    }
---
+++ taxidriversafetyapp/handler.py
@@ -0,0 +1,89 @@
+import json
+import os
+import sys
+import concurrent.futures
+import time
+
+import requests
+
+gateway_hostname = os.getenv("gateway_hostname", "gateway.openfaas")
+base_url = "http://" + gateway_hostname + ":8080/function/"
+def call_request(req, f):
+    try:
+        r = requests.get(base_url + f, data=req)
+        if r.status_code != 200:
+            returnBadRequest('Looks like something went wrong while requesting ' + f);
+        else:
+            return {
+                "statusCode": r.status_code,
+                "body": r.json()
+            }
+    except Exception as e:
+        return returnBadRequest('Looks like something went wrong: ' + str(e))
+
+
+
+def handle(event, context):
+
+    # 1. call human detector
+    # 2. if no human found
+    # 2.1 return true (driver is safe)
+    # 3. if human found
+    # 3.1 call mask detection
+    # 3.2 call object detection
+    # 4. return wear a mask && no weapon detected
+
+    start_time = time.time()
+    if not event.body:
+        return returnBadRequest("Error: No image.")
+    # 1. call human detector
+    r = call_request(event.body, "humandetection")
+    if r['statusCode'] != 200:
+        return r
+    results = []
+
+    try:
+    # 2. no human found
+        if not r['body']['found']:
+            isSave = True
+        # 3. human found
+        else:
+        # 3.1 Call mask detection
+        # 3.2 Call gun detection
+            urls = ["gundetection", "maskdetection"]
+            isSave = True
+            with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+                future_to_url = {executor.submit(call_request, event.body, url) for url in urls}
+                for future in concurrent.futures.as_completed(future_to_url):
+                    try:
+                        data = future.result()
+                        if data['statusCode'] != 200:
+                            return data
+                        results.append(data)
+                        if 'masks' in data['body']:
+                            if "bare" in data['body']['masks']:
+                                isSave = False
+                        if 'gunExist' in data['body']:
+                            isSave = not data['body']['gunExist']
+
+                    except Exception as e:
+                        return returnBadRequest('Looks like something went wrong: ' + str(e))
+
+    except Exception as e:
+        return returnBadRequest('Looks like something went wrong: ' + str(e) + ' ' + str(r))
+    stop_time = time.time()
+
+    return {
+        "statusCode": 200,
+        "body": {
+            "results": results,
+            "total_duration": (stop_time - start_time) * 1000,
+            "isSave": isSave
+        }
+    }
+
+def returnBadRequest(msg):
+    return {
+        "statusCode": 400,
+        "body": msg
+    }
