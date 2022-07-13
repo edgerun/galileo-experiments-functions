185cbf5fa73f23ff3736054f3e35d17702611227
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2022-07-13 22:46:12 +0100

    Fix build script and add release script for objectdetection

--- bin/objectdetection/build.sh
+++ bin/objectdetection/build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 ARCH="${1:-amd64}"
+
 image=edgerun/objectdetection
 version=$(git rev-parse --short HEAD)
-
+tflite_version=2.1.0
 basetag="${image}:${version}"
 
-# change into project root
-# BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# PROJECT_ROOT=$(realpath "${BASE}/../../../")
-# cd $PROJECT_ROOT
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+export DOCKER_BUILD_KIT=1
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
 tmp="$(mktemp)"
 
 # scaffold build folder
@@ -20,18 +22,19 @@ cd ./build/objectdetection || exit
 if [[ "$ARCH" == "amd64" ]]
 then
   echo "Build amd64"
-  docker build -t ${basetag}-amd64 --build-arg WATCHDOG_VERSION=1.0.0-amd64  .
+  docker build -t ${basetag}-amd64 --build-arg WATCHDOG_VERSION=1.0.0-amd64  --build-arg TFLITE_VERSION=${tflite_version}-amd64 .
 elif [[ "$ARCH" == "arm32v7" ]]
 then
   echo "Build arm32v7"
-  docker build -t ${basetag}-arm32v7 --build-arg WATCHDOG_VERSION=1.0.0-arm32v7  .
+  docker build -t ${basetag}-arm32v7 --build-arg WATCHDOG_VERSION=1.0.0-arm32v7  --build-arg TFLITE_VERSION=${tflite_version}-arm32v7 .
 elif [[ "$ARCH" == "arm64v8" ]]
 then
   echo "Build arm64v8"
-  docker build -t ${basetag}-arm64v8 --build-arg WATCHDOG_VERSION=1.0.0-arm64v8 .
+  docker build -t ${basetag}-arm64v8 --build-arg WATCHDOG_VERSION=1.0.0-arm64v8 --build-arg TFLITE_VERSION=${tflite_version}-arm64 .
 else
   echo "Unknown arch: $ARCH"
 fi
 
 cd ../../
 rm "${tmp}"
+
