34bbfd8c5e4f17bfd648cc11b2ef6c47a9d71276
Paul Prüller <>
2022-07-15 12:07:27 +0200

    fix download models script

--- bin/download_models.sh
+++ bin/download_models.sh
@@ -15,20 +15,19 @@ cd ./maskdetection
 mkdir model
 cd ..
 #download model for poseestimation
-wget https://drive.google.com/file/d/149EikDB4v38tIFRBj0eznq5u1FPjFBEe/view?usp=sharing -O ./poseestimation/model/poseestimation.caffemodel
-wget https://drive.google.com/file/d/1xVEDxkHJlaN_R3S3Y3N6y7ux3f-tS5Uu/view?usp=sharing -O ./poseestimation/model/poseestimation.prototxt
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=149EikDB4v38tIFRBj0eznq5u1FPjFBEe' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=149EikDB4v38tIFRBj0eznq5u1FPjFBEe" -O ./poseestimation/model/poseestimation.caffemodel && rm -rf /tmp/cookies.txt
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1xVEDxkHJlaN_R3S3Y3N6y7ux3f-tS5Uu" -O ./poseestimation/model/poseestimation.prototxt
 
 #download model for sleepdetection
-wget https://drive.google.com/file/d/1mpA0H53frYFm7QCFxMsaRxaEPmpEkSvW/view?usp=sharing -O ./sleepdetection/model/sleepdetection.dat
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1mpA0H53frYFm7QCFxMsaRxaEPmpEkSvW" -O ./sleepdetection/model/sleepdetection.dat
 
 #download model for maskdetection
-wget https://drive.google.com/file/d/1EJk3kfn548mpjIt2wEqNvujG3ZwQbAGm/view?usp=sharing -O ./maskdetection/model/maskdetection.tflite
-wget https://drive.google.com/file/d/1pjNV1eKYzq3ZRrm0hcUD-LMx3o92tCW8/view?usp=sharing -O ./maskdetection/model/maskdetection.txt
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1EJk3kfn548mpjIt2wEqNvujG3ZwQbAGm" -O ./maskdetection/model/maskdetection.tflite
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1pjNV1eKYzq3ZRrm0hcUD-LMx3o92tCW8" -O ./maskdetection/model/maskdetection.txt
+
 
 #download model for gundetection
-wget https://drive.google.com/file/d/1EJk3kfn548mpjIt2wEqNvujG3ZwQbAGm/view?usp=sharing -O ./gundetection/model/gundetection.xml
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=13PoX1xCl4z7Fe2KfUwS4MtmhcBPXHBLJ" -O ./gundetection/model/gundetection.xml
 
 #download model for objectdetection
-wget https://drive.google.com/file/d/1aJZeq2o5U-rAGC2p-0I3hpSahfmyS2nX/view?usp=sharing -O ./objectdetection/model/objectdetection.tflite
-wget https://drive.google.com/file/d/1nUoym9zaXOTez83ZQy0OzqJoAxCtNtde/view?usp=sharing -O ./objectdetection/model/objectdetection.xml
-
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1a4gTfJOoMZHFy9WOhslmGs9saowOau4X" -O ./objectdetection/model/objectdetection.tflite
