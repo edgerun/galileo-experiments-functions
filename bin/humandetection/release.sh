ddc239dfa060147d415562a86293ff89ee6e4c1c
Paul Prüller <>
2022-07-15 14:40:39 +0200

    add humandetection scripts

---
+++ bin/humandetection/release.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+ARCH="${1:-amd64}"
+
+image=edgerun/humandetection
+version=$(git rev-parse --short HEAD)
+tflite_version=2.1.0
+basetag="${image}:${version}"
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+export DOCKER_BUILD_KIT=1
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+tmp="$(mktemp)"
+
+# scaffold build folder
+sed "s/VERSION/$version/g" humandetection.yml >"${tmp}"
+faas-cli build --shrinkwrap -f "${tmp}"
+cd ./build/humandetection || exit
+
+# build container
+echo "Build amd64"
+docker build -t ${basetag}-amd64 --build-arg WATCHDOG_VERSION=1.0.0-amd64  --build-arg TFLITE_VERSION=${tflite_version}-amd64 .
+
+echo "Build arm32v7"
+docker build -t ${basetag}-arm32v7 --build-arg WATCHDOG_VERSION=1.0.0-arm32v7  --build-arg TFLITE_VERSION=${tflite_version}-arm32v7 .
+
+echo "Build arm64v8"
+docker build -t ${basetag}-arm64v8 --build-arg WATCHDOG_VERSION=1.0.0-arm64v8 --build-arg TFLITE_VERSION=${tflite_version}-arm64 .
+
+cd ../../
+rm "${tmp}"
+
+
+# push em all
+docker push ${basetag}-amd64 &
+docker push ${basetag}-arm32v7 &
+docker push ${basetag}-arm64v8 &
+
+wait
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+# create the manifest
+docker manifest create -a ${basetag} \
+  ${basetag}-amd64 \
+  ${basetag}-arm32v7 \
+  ${basetag}-arm64v8
+
+# explicit annotations
+docker manifest annotate ${basetag} ${basetag}-arm64v8 --os "linux" --arch "arm64" --variant "v8"
+docker manifest annotate ${basetag} ${basetag}-arm32v7 --os "linux" --arch "arm" --variant "v7"
+
+# ship it
+docker manifest push --purge ${basetag}
