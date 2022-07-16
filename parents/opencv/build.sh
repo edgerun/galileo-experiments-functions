1f6ba987428d03a1e7d61da36b241c39b66040df
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2022-07-16 17:05:51 +0100

    Update opencv parent scripts

--- parents/opencv/build.sh
+++ parents/opencv/build.sh
@@ -3,6 +3,6 @@ VERSION=${1:-2.1.0}
 
 cd ./docker
 
-docker build -t pruellerpaul/opencv:"${VERSION}"-arm64v8 -f Dockerfile.opencv.arm64v8 .
-docker build -t pruellerpaul/opencv:"${VERSION}"-amd64 -f Dockerfile.opencv.amd64 .
-docker build -t pruellerpaul/opencv:"${VERSION}"-arm32v7 -f Dockerfile.opencv.arm32v7 .
+docker build -t edgerun/opencv:"${VERSION}"-arm64v8 -f Dockerfile.opencv.arm64v8 .
+docker build -t edgerun/opencv:"${VERSION}"-amd64 -f Dockerfile.opencv.amd64 .
+docker build -t edgerun/opencv:"${VERSION}"-arm32v7 -f Dockerfile.opencv.arm32v7 .
